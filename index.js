886e6ffb122441eec7c26045c6efdeba0fdc3d16
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-14 19:47:25 -0800

    commit tree a967ce5b55c1db852447c25aecc77fecc0e811a7

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const locatedNumbers = {};
+  for (let number of array) {
+    let secondNumber = target - number;
+    if (locatedNumbers[secondNumber]) return true;
+    locatedNumbers[number] = true;
+  }
+  return false
 }
 
 /* 
@@ -7,7 +13,13 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+Iterate over the array of numbers
+  For the current number, identify a second number that adds to our target
+  (For example: if our number is 2, and the target is 5, the second number is 3)
+  Iterate over the remaining numbers in the array
+    Check if any of the remaining numbers is the second
+      If so, return true
+If we reach the end of the array, return false
 */
 
 /*
@@ -32,3 +44,35 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+}
